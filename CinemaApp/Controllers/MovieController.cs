c1f3a9c6de04284f6164cad5c9c2a77372a35248
Noor <noorulainibrahim75@gmail.com>
2024-12-19 15:18:00 +0400

    model and controller added for movie

--- CinemaApp/Controllers/MovieController.cs
+++ CinemaApp/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CinemaApp.Models;
 
 namespace CinemaApp.Controllers
 {
@@ -11,7 +12,20 @@ namespace CinemaApp.Controllers
         // GET: Movie
         public ActionResult Index()
         {
-            return View();
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "The Shawshank Redemption",
+                Genre = "Drama",
+                Director = "Frank Darabont",
+                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                ReleaseDate = new DateTime(1994, 10, 14),
+                Duration = 142,
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+                TrailerUrl = "https://www.youtube.com/watch?v=6hB3S9bIaco"
+            };
+
+            return View(movie);
         }
     }
 }
