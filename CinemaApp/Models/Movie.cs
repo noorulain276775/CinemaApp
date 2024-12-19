0ff059e7df310141cea2fbde0601370889c33083
Noor <noorulainibrahim75@gmail.com>
2024-12-19 14:11:57 +0400

    Add project files.

---
+++ CinemaApp/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public string TrailerUrl { get; set; }
+    }
+}
